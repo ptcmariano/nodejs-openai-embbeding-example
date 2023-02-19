14a3028f47f43c19f0aedef35020e5afdad4b482
Paulo Mariano <paulotiagocm@hotmail.com>
2023-02-19 09:59:28 -0300

    feat: find all pages and read

--- webcrawler.js
+++ webcrawler.js
@@ -4,25 +4,51 @@ const domain = 'https://criancafelizdesorocaba.org.br/';
 
 let pagesToSearch = [];
 
-const c = new Crawler({
+const crawl = new Crawler({
     rateLimit: 1000,
     callback: (error, res, done) => {
         if (error) {
             console.log(error);
         } else {
             const $ = res.$;
-            $("body").find("a[href^='" + domain + "']").each(function(i, link){
-                const samelink = pagesToSearch.find(search => search == $(link).attr('href'));
-                console.log($(link).attr('href'), samelink)
-                if (!samelink) {
-                    pagesToSearch.push($(link).attr('href'));
-                }
-                // todo: fill pages read
-            });
+            searchLinksToRead($);
+            queuePagesToRead();
+            // todo: get all content
         }
         done();
     }
 });
 
-c.queue(domain);
-// todo: function to get more pages to read
+crawl.queue(domain);
+
+const searchLinksToRead = ($) => {
+    try {
+        $("body").find("a[href^='" + domain + "']").each(function(i, link){
+            // no search pdf and mp4
+            if ($(link).attr('href').indexOf('.mp4') > 0 || 
+                $(link).attr('href').indexOf('.pdf') > 0) {
+                return;
+            }
+            // find same link in pages to search
+            const samelink = pagesToSearch.find(search => search.link == $(link).attr('href'));
+            if (!samelink) {
+                pagesToSearch.push({
+                    'link': $(link).attr('href'),
+                    read: false
+                });
+            }
+        });
+    } catch (error) {
+        // ignore pages cant search
+    }
+}
+
+const queuePagesToRead = () => {
+    pagesToSearch.map((page,index) => {
+        if (!page.read) {
+            console.log('read page: '+page.link)
+            pagesToSearch[index].read = true;
+            crawl.queue(page.link);
+        }
+    });
+}
